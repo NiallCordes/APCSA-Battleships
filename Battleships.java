c7114512d5979bb395fc9bb67b7a4fd676b8822a
NiallCordes <78307000+NiallCordes@users.noreply.github.com>
2021-04-16 21:49:27 +0800

    Add files via upload
    Implemented a scanner and introductory statements in main class

--- Battleships.java
+++ Battleships.java
@@ -1,8 +1,14 @@
 import java.util.*;
 
-public class Battleships extends Ships{
-    public Battleships() {
-        length = 4
-        symbol = Node.B
-    }
-}
+public class Battleships {
+  public static void main(String[] args) {
+
+    // initialze board and scanner
+    Board gameBoard = new Board();
+    Scanner scan = new Scanner(System.in);
+
+    // introduction method and function
+    System.out.println("Welcome to Battleships!");
+    System.out.println("The ships have been placed on the board by the computer.");
+    gameBoard.printPlayerViewBoard();
+
