4f4508a9ab58108568f836901f7471e172110421
NiallCordes <78307000+NiallCordes@users.noreply.github.com>
2021-04-16 22:07:53 +0800

    Add files via upload
    created both the cruiser and destroyer ships in separate files as a simple child class to store symbols and values like lengths

---
+++ Cruiser.java
@@ -0,0 +1,6 @@
+public class Cruiser extends Ships {
+    public Cruiser() {
+      length = 3;
+      symbol = Node.C;
+    }
+  }
---
+++ Destroyer.java
@@ -0,0 +1,6 @@
+public class Destroyer extends Ships {
+    public Destroyer() {
+      length = 2;
+      symbol = Node.D;
+    }
+  }
